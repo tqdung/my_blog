19568e212a4a7875ec015680b3d85192dcdfa259
Tran Quoc Dung <dung.tq@gnt.com.vn>
2019-03-14 17:10:27 +0700

    [update] mobile menu

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,60 +1,84 @@
 <template>
-    <header id="header_top">
-        <b-container>
-            <b-row>
-                <b-col sm="6" cols="12">
-                    <div class="header-left">
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Travel</a></li>
-                            <li><a href="#">Eat</a></li>
-                            <li><a href="#">Relax</a></li>
-                            <li><a href="#">Videos</a></li>
-                        </ul>
-                    </div>
-                </b-col>
-                <b-col sm="6" cols="12">
-                    <div class="header-right">
-                        <ul>
-                            <li><a href="#"><img :src="require('@/assets/images/icons/facebook.webp')" alt="facebook"></a></li>
-                            <li><a href="#"><img :src="require('@/assets/images/icons/instagram.webp')" alt="instagram"></a></li>
-                            <li><a href="#"><img :src="require('@/assets/images/icons/pinterest.webp')" alt="pintrest"></a></li>
-                            <li><a href="#"><img :src="require('@/assets/images/icons/twister.webp')" alt="twister"></a></li>
-                        </ul>
-                        <div class="toggle" @click="openMenu = !openMenu">
-                            <i class="fa fa-bars"></i>
-                        </div>
-                    </div>
-                </b-col>
-            </b-row>
-        </b-container>
-        <div class="mobile-menu" :class="{show: openMenu}">
-            <div  @click="openMenu =  false" class="placeholder"></div>
-            <div class="menu">
-              <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Travel</a></li>
-                <li><a href="#">Eat</a></li>
-                <li><a href="#">Relax</a></li>
-                <li><a href="#">Videos</a></li>
-              </ul>
+  <header id="header_top">
+    <b-container>
+      <b-row>
+        <b-col sm="6" cols="12">
+          <div class="header-left">
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Travel</a>
+              </li>
+              <li>
+                <a href="#">Eat</a>
+              </li>
+              <li>
+                <a href="#">Relax</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+            </ul>
+          </div>
+        </b-col>
+        <b-col sm="6" cols="12">
+          <div class="header-right">
+            <div class="toggle" @click="openMenu = !openMenu">
+              <i class="fa fa-bars"></i>
             </div>
-        </div>
-    </header>
+            <ul>
+              <li>
+                <a href="#">
+                  <img :src="require('@/assets/images/icons/facebook.webp')" alt="facebook">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img :src="require('@/assets/images/icons/instagram.webp')" alt="instagram">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img :src="require('@/assets/images/icons/pinterest.webp')" alt="pintrest">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img :src="require('@/assets/images/icons/twister.webp')" alt="twister">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+    <mobile-menu v-bind:openMenu="openMenu" @closeMenu="closeMenu"></mobile-menu>
+  </header>
 </template>
 
 <script>
+import MobileMenu from './MobileMenu.vue'
 export default {
+  components: {
+    MobileMenu
+  },
   data () {
     return {
       openMenu: false
+    }
+  },
+  methods: {
+    closeMenu () {
+      this.openMenu = false
     }
   }
 }
 </script>
 
 <style>
-
 </style>
---
+++ src/components/layouts/MobileMenu.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="mobile-menu" :class="{show: openMenu}">
+    <div class="outside" @click="toggle"></div>
+    <div class="menu">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['openMenu'],
+  data () {
+    console.log(this.openMenu)
+    return {}
+  },
+  methods: {
+    toggle () {
+      console.log('fafd')
+      this.$emit('closeMenu', null)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/common.scss";
+#mobile-menu {
+  pointer-events: none;
+  //   display: none;
+  //   &.show {
+  //     display: block;
+  //   }
+  .menu {
+    top: 0;
+    left: -80vw;
+    width: 80vw;
+    height: 100vh;
+    background-color: $background-menu;
+    position: absolute;
+    transition: left 0.3s ease-in-out;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    ul {
+      display: flex;
+      flex-direction: column;
+      padding-left: 25px;
+      font-size: 18px;
+      font-weight: bold;
+      line-height: 36px;
+    }
+
+    a {
+      color: white;
+    }
+    :focus {
+      color: #436ca8;
+    }
+  }
+
+  .outside {
+    width: 100vw;
+    height: 100vh;
+    left: 0;
+    top: 0;
+    position: fixed;
+    background-color: transparent;
+    transition: background-color 0.3s linear;
+  }
+  &.show {
+    pointer-events: initial;
+    .outside {
+      background: #0003;
+    }
+    .menu {
+      left: 0;
+    }
+  }
+}
+</style>
